bcf5182d907a56b347a1279a16683df7e0a82280
Gagan740 <gagan@codehelperz.com>
2017-08-15 19:34:10 +0530

    myApp

---
+++ server.js
@@ -0,0 +1,33 @@
+var express 		=	require('express');
+var app 			=	express();
+var port 			=	process.env.PORT || 8080;
+var morgan    	=	require('morgan');
+var mongoose	=	require('mongoose');
+var User 			=	require('./app/models/user');
+var bodyParser 	=	require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost:27017/MEAN',(err)=>{
+	if(err){
+		console.log(`Not connected to the database ${err}.`)
+	}else{
+		console.log('Successfully connected to MongoDB.')
+	}
+});
+
+app.post('/users',function(req,res){
+	var user 			=	new User();
+	user.username 	=	req.body.username;
+	user.password 	=	req.body.password;
+	user.email		=	req.body.email;
+	user.save();
+	res.send('User creadted.')
+})
+
+app.listen(port,function(){
+	console.log(`Running the server on port ${port}.`);
+});
