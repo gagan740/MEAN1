01ee0dba0b1123620bba9336723b440fae14b823
Gagan740 <gagan@codehelperz.com>
2017-08-15 19:42:20 +0530

    updates

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express 		=	require('express');
-var app 			=	express();
-var port 			=	process.env.PORT || 8080;
+var express 	=	require('express');
+var app 		=	express();
+var port 		=	process.env.PORT || 8080;
 var morgan    	=	require('morgan');
 var mongoose	=	require('mongoose');
-var User 			=	require('./app/models/user');
+var User 		=	require('./app/models/user');
 var bodyParser 	=	require('body-parser');
 
 app.use(bodyParser.json());
